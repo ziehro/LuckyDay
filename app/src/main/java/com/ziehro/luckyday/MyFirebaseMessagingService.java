a0ff47032c86809b95eb9525b9c44358b4189f77
ziehro <56759812+ziehro@users.noreply.github.com>
2022-01-25 15:17:40 -0700

    first push notifications 2

--- app/src/main/java/com/ziehro/luckyday/MyFirebaseMessagingService.java
+++ app/src/main/java/com/ziehro/luckyday/MyFirebaseMessagingService.java
@@ -165,7 +165,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder =
                 new NotificationCompat.Builder(this, channelId)
-                        .setSmallIcon(R.drawable.ic_stat_ic_notification)
+                        .setSmallIcon(R.drawable.greenlightbutton)
                         .setContentTitle(getString(R.string.fcm_message))
                         .setContentText(messageBody)
                         .setAutoCancel(true)
